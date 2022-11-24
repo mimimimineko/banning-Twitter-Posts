e82856308e0b9d7d00d01da2da8d8960344045c2
mimimimineko <sleepsnoop1207@gmail.com>
2022-11-24 15:22:06 +0900

    モバイル表示に対応 ツイートボタンが消えない不具合を解消

--- content_script.js
+++ content_script.js
@@ -2,15 +2,24 @@ function remove(){
     try{
         document.querySelector(".css-1dbjc4n.r-kemksi.r-oyd9sg").remove()
         console.log("低浮上アシスタント : 入力欄を消して差し上げましたわ")
-        document.querySelector(".css-1dbjc4n.r-1awozwy.r-1r5su4o.r-e7q0ms").remove()
-        console.log("ツイートボタンを消して差し上げましたわ")
-        console.log("低浮上アシスタント : 処理完了ですわ")
     }catch(e){
-        console.log("低浮上アシスタント : " + e)
+        console.log("低浮上アシスタント : あら、もう既に入力欄は消えているようですわ   " + e)        
     }
+    try{
+        document.querySelector(".css-1dbjc4n.r-1r5su4o.r-e7q0ms").remove()
+        console.log("低浮上アシスタント : ツイートボタンを消して差し上げましたわ")
+    }catch(e){
+        try{
+            document.querySelector(".css-1dbjc4n.r-173mn98.r-18rd0c5.r-1ff5aok").remove()
+            console.log("低浮上アシスタント : モバイル表示にも対応してますのよ")
+        }catch(e){
+            console.log("低浮上アシスタント : ツイートボタン？そんなものありませんわ。  " + e)
+        }
+    }
+    console.log("低浮上アシスタント : 処理完了ですわ")
 }
 
 
 console.log("低浮上アシスタント : content_script.jsが実行されました")
 setTimeout(function(){remove()},1000)
-setInterval(function(){remove()},10000)
+setInterval(function(){remove()},5000)
